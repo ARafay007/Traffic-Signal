1ca096cc3a90249fed60a427aab14c0a731d4daa
ARafay007 <rafay_007@Outlook.com>
2021-07-29 00:10:14 -0700

    Four traffic signals system

--- script.js
+++ script.js
@@ -1,26 +1,52 @@
 'use strict';
 
-const redLight = document.querySelector('.red');
-const yellowLight = document.querySelector('.yellow');
-const greenLight = document.querySelector('.green');
+const redLight = document.querySelectorAll('.red');
+const yellowLight = document.querySelectorAll('.yellow');
+const greenLight = document.querySelectorAll('.green');
+
+const redLight2 = document.querySelectorAll('.red2');
+const yellowLight2 = document.querySelectorAll('.yellow2');
+const greenLight2 = document.querySelectorAll('.green2');
+
 let count = 1, timer = 0;
 
 const changeLights = () => {
     if(count === 1){
-        redLight.style.backgroundColor = 'red';
-        yellowLight.style.backgroundColor = greenLight.style.backgroundColor = 'white';
+        redLight.forEach(el => el.style.backgroundColor = 'red');
+        yellowLight.forEach(el => el.style.backgroundColor = 'white');
+        greenLight.forEach(el => el.style.backgroundColor = 'white');
+
+        redLight2.forEach(el => el.style.backgroundColor = 'white');
+        yellowLight2.forEach(el => el.style.backgroundColor = 'white');
+        greenLight2.forEach(el => el.style.backgroundColor = 'green');
+
         count++;
         timer = 3000;
     } else if(count === 2){
-        yellowLight.style.backgroundColor = 'yellow';
-        redLight.style.backgroundColor = greenLight.style.backgroundColor = 'white';
+        redLight.forEach(el => el.style.backgroundColor = 'white');
+        yellowLight.forEach(el => el.style.backgroundColor = 'yellow');
+        greenLight.forEach(el => el.style.backgroundColor = 'white');
+
         count++;
         timer = 1000;
     } else if(count === 3){
-        greenLight.style.backgroundColor = 'green';
-        redLight.style.backgroundColor = yellowLight.style.backgroundColor = 'white';
-        count = 1;
+        redLight.forEach(el => el.style.backgroundColor = 'white');
+        yellowLight.forEach(el => el.style.backgroundColor = 'white');
+        greenLight.forEach(el => el.style.backgroundColor = 'green');
+
+        redLight2.forEach(el => el.style.backgroundColor = 'red');
+        yellowLight2.forEach(el => el.style.backgroundColor = 'white');
+        greenLight2.forEach(el => el.style.backgroundColor = 'white');
+
+        count++;
         timer = 3000;
+    } else if(count === 4){
+        redLight2.forEach(el => el.style.backgroundColor = 'white');
+        yellowLight2.forEach(el => el.style.backgroundColor = 'yellow');
+        greenLight2.forEach(el => el.style.backgroundColor = 'white');
+
+        count=1;
+        timer = 1000;
     }
 };
 
